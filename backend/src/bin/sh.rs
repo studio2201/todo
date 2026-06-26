4f5fbb41256df6aa0c12f369beeb96b48bc9d16d
jeryd <jerydleuck@gmail.com>
2026-06-25 21:38:34 -1000

    style: delegate themes/print CSS and sh console alert to shared-assets

--- backend/src/bin/sh.rs
+++ backend/src/bin/sh.rs
@@ -1,25 +1,7 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    // Clear screen
-    print!("\x1B[2J\x1B[1;1H");
-
-    println!(
-        r#" _______________________________________
-/ I'm sorry, Dave. I'm afraid Nix won't \
-\ let me do that.                       /
- ---------------------------------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-                ||----w |
-                ||     ||"#
-    );
-
-    println!("\x1B[1;31m\nSystem Alert: Console Access is UNAUTHORIZED.\x1B[0m");
-    println!("This application is running inside a secure, read-only Nix container.");
-    println!("Direct shell access is disabled for environment isolation and security.");
-    println!("\nPress \x1B[1;37m[Enter]\x1B[0m to close connection...");
+    ubermetroid_shared::print_unauthorized_console_message();
 
     let stdin = io::stdin();
     let mut buffer = String::new();
